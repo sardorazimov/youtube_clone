3864f40d615b1d9a3ecfb912eac0c714fd8feb1d
sardorazimov <sardorazimov2901@gmail.com>
2024-10-01 23:11:28 +0300

    hero

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import Hero from '@/components/shared/detalis/Hero'
 import { Navigation } from '@/components/shared/Navigation'
 import React from 'react'
 
@@ -5,6 +6,7 @@ const page = () => {
   return (
     <div>
       <Navigation />
+      <Hero />
     </div>
   )
 }
--- components/shared/detalis/Hero.tsx
+++ components/shared/detalis/Hero.tsx
@@ -2,7 +2,46 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='flex w-full flex-col h-screen py-10'>
+     <div className=' w-full  grid grid-cols-1 md:grid-cols-3 gap-10'>
+      <div  className='rounded  overflow-hidden shadow-lg' >
+      <a href=""></a>
+      <div>
+      <iframe width="" height="" src="https://www.youtube.com/embed/eDHzY9jjjGg?list=RDeDHzY9jjjGg" title="UZmir &amp; Mira - Izlamading (MooD video) | Узмир &amp; Мира - Изламадинг" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+      </div>
+      <div  className='rounded  overflow-hidden shadow-lg' >
+      <a href=""></a>
+      <div>
+      <iframe width="" height="" src="https://www.youtube.com/embed/eDHzY9jjjGg?list=RDeDHzY9jjjGg" title="UZmir &amp; Mira - Izlamading (MooD video) | Узмир &amp; Мира - Изламадинг" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+      </div>
+      <div  className='rounded  overflow-hidden shadow-lg' >
+      <a href=""></a>
+      <div>
+      <iframe width="" height="" src="https://www.youtube.com/embed/eDHzY9jjjGg?list=RDeDHzY9jjjGg" title="UZmir &amp; Mira - Izlamading (MooD video) | Узмир &amp; Мира - Изламадинг" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+      </div>
+      <div  className='rounded  overflow-hidden shadow-lg' >
+      <a href=""></a>
+      <div>
+      <iframe width="" height="" src="https://www.youtube.com/embed/eDHzY9jjjGg?list=RDeDHzY9jjjGg" title="UZmir &amp; Mira - Izlamading (MooD video) | Узмир &amp; Мира - Изламадинг" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+      </div>
+      <div  className='rounded  overflow-hidden shadow-lg' >
+      <a href=""></a>
+      <div>
+      <iframe width="" height="" src="https://www.youtube.com/embed/eDHzY9jjjGg?list=RDeDHzY9jjjGg" title="UZmir &amp; Mira - Izlamading (MooD video) | Узмир &amp; Мира - Изламадинг" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+      </div>
+      <div  className='rounded  overflow-hidden shadow-lg' >
+      <a href=""></a>
+      <div>
+      <iframe width="" height="" src="https://www.youtube.com/embed/eDHzY9jjjGg?list=RDeDHzY9jjjGg" title="UZmir &amp; Mira - Izlamading (MooD video) | Узмир &amp; Мира - Изламадинг" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+      </div>
+     </div>
+    </div>
   )
 }
 
